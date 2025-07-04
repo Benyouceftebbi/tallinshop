28042d7e471c93463f632399aceb85dbefcc16d1
Benyouceftebbi <benyoucef.tebbi@std.antalya.edu.tr>
2025-07-04 11:19:19 +0100

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next/metadata"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import  { FacebookPixelEvents } from "@/hooks/facebook-pixel"
-import { Suspense } from "react"
+
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,15 +21,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
- 
+    <head>
+        {/* ✅ Meta Pixel Script */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1189512162479638');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
 
           
           {children}
-           <Suspense fallback={null}>
-          <FacebookPixelEvents />
-        </Suspense>
+  
         </ThemeProvider>
       </body>
     </html>
